b02fabacdf8e5d9a33b40b0a65f1e1914879db77
liufang8 <liubangzaishi@sina.com>
2016-12-08 08:08:50 +0800

    错误处理 ajax

---
+++ example/crossdomain/postMessage/compute.js
@@ -0,0 +1,5 @@
+﻿var i, sum;
+for (i = 0, sum = 0; i < 100000; i++) {
+	sum += i;
+}
+postMessage(sum);
---
+++ service/php/userinfo.php
@@ -0,0 +1,19 @@
+﻿<?php 
+	header('Content-Type: text/plain');
+	// $query_string = "";
+	//  if ($_POST) {
+	//    $kv = array();
+	//    foreach ($_POST as $key => $value) {
+	//      $kv[] = "$key=$value";
+	//    }
+	//    $query_string = join("&", $kv);
+	//  }
+	//  else {
+	//    $query_string = $_SERVER['QUERY_STRING'];
+	//  }
+	//  echo $query_string;
+
+
+	echo 'aaaaa';
+
+?>
